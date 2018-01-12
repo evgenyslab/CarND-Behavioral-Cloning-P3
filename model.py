98250aba2ce89857a3752d3c2a5eeba68913ed3f
evgenyslab <e.nuger@gmail.com>
2018-01-12 11:29:44 -0500

    report

--- model.py
+++ model.py
@@ -1,67 +1,149 @@
+# IMPORTS
+%matplotlib inline
 import os
 import csv
 import cv2
 import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 import sklearn
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Convolution2D, Lambda, Cropping2D, Dropout
+print('done')
 
-
+#==================================================================================================
 # FUNCTIONAL DEFINITIONS
-def generator(samples, batch_size=32, use_side_images = False, steer_correction = 0.2):
+def loadImage(name):
+    if not os.path.isfile(name):
+        raise ValueError('File "{}" as {} Does not exist!'.format(name))
+    return cv2.cvtColor(cv2.imread(name),cv2.COLOR_BGR2RGB)
+
+#==================================================================================================
+# Generator for image loading:
+def generator(samples, batch_size=32, use_side_images = False, steer_correction = 0.2, basepath = 'data/IMG/'):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             angles = []
-            
-
             for batch_sample in batch_samples:
-                name = 'Data/IMG/'+batch_sample[0].split('/')[-1]
-                image_center = cv2.imread(name)
+                name = basepath+batch_sample[0].split('/')[-1]
+                # Load & Append image to list:
+                images.append(loadImage(name))
+                
                 steering_center = float(batch_sample[3])
-                # append to list:
-                images.append(image_center)
                 angles.append(steering_center)
                 # if using with side images:
                 if use_side_images:
                     # create adjusted steering measurements for the side camera images
-                    steering_left = steering_center + steer_correction
+                    steering_left = steering_center + steer_correction                    
+                    
+                    name = basepath+batch_sample[1].split('/')[-1]
+                    images.append(loadImage(name))
+                    angles.append(steering_left)
+                    
                     steering_right = steering_center - steer_correction
-                    img_left = process_image(np.asarray(Image.open(path + row[1])))
-                    img_right = process_image(np.asarray(Image.open(path + row[2])))
+                    name = basepath+batch_sample[2].split('/')[-1]
+                    images.append(loadImage(name))
+                    angles.append(steering_right)
+                    pass
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
-            
 
+print('done')
+
+#==================================================================================================
 # Data Loading
 samples = []
-with open('Data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
+# with open('data/driving_log.csv') as csvfile:
+# find all .csv files in directory and load each one:
+folder = '/home/en/Desktop/Training/'
+basepath = folder + 'IMG/'
+files = 0
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        with open(folder + file) as csvfile:
+            files +=1
+            reader = csv.reader(csvfile)
+            cc = 0
+            for line in reader:
+                if (cc!= 0):
+                    samples.append(line)
+                cc+=1
 
 # Partition Data
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+# Print out some data:
+print('Total files read: {}'.format(files))
+print('Total Data points: {}'.format(len(samples)))
+print('Training Samples: {}\nValidation Samples: {}'.format(len(train_samples),len(validation_samples)))
+
+# VARIABLE SETUP:
+batch_size = 32
+epochs = 3
+
+print('done')
+#==================================================================================================
+# test input image from generator:
+test_generator = generator(train_samples, batch_size=1, basepath = basepath)
+[X_train, y_train] = next(test_generator)
+
+# plot to show image grabbed from generator:
+fig = plt.figure(figsize = (3, 3))
+axis = fig.add_subplot(1, 1, 1, xticks=[], yticks=[])
+axis.imshow(X_train[0])
+plt.show()
+
+input_shape = X_train[0].shape
+print(input_shape)
+
+
+#==================================================================================================
+# Training:
+input_shape = (160,320,3)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=batch_size, use_side_images = True,  basepath = basepath)
+validation_generator = generator(validation_samples, batch_size=batch_size, use_side_images = True,  basepath = basepath)
+
+print(len(train_samples))
+print(len(validation_samples))
 
 ch, row, col = 3, 80, 320  # Trimmed image format
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: x/127.5 - 1.,input_shape=(ch, row, col),output_shape=(ch, row, col)))
-model.add(... finish defining the rest of your model architecture here ...)
+model.add(Lambda(lambda x: x/127.5 - 1.,input_shape=input_shape))
+# cropping:
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+# NVidia architecture:
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dropout(0.7))
+model.add(Dense(50))
+model.add(Dropout(0.85))
+model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= /
-            len(train_samples), validation_data=validation_generator, /
-            nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator,
+                    samples_per_epoch = 3*len(train_samples),
+                    validation_data =validation_generator, 
+                    nb_val_samples =len(validation_samples),
+                    nb_epoch=5,
+                    verbose = 1)
+
+model.save('model.h5')
+print('Model Saved!')
--- report.md
+++ report.md
@@ -1,12 +1,50 @@
+# **Behavioral Cloning** 
+---
 
-used nvidia framework with image cropping & 3 image sets.
-train with vanilla, seemed to work - may be over fitting
-gathered the following data for retraining:
-2 loops forward driven well + 1 loop with swerving recovery
-same as above but in reverse.
+**Behavioral Cloning Project**
 
-Trained model with vanila data works well on the track, most likely overfits.
-Trained model with new data results in more car sway which would be due to the reverse runs.
-low resolution of images suggest the sway occurs due to networks attempt to locate the shoulder of the road, while vanila model just encodes the whole track.
+The goals / steps of this project are the following:
+* Use the simulator to collect data of good driving behavior
+* Build, a convolution neural network in Keras that predicts steering angles from images
+* Train and validate the model with a training and validation set
+* Test that the model successfully drives around track one without leaving the road
+* Summarize the results with a written report
+
+## Model Architecture and Training Strategy
+
+### Architecture
+The model architecture was based on the highly acclaimed [NVidia model](https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/). The input layer to the model consisted of a normalized image that was cropped with a Keras cropping function to remove the sky and car hood.
+
+| Layer | Description|
+|-------|-----------:|
+| Input Image | Normalize|
+| Crop | Row cropping|
+| Convolution | Depth 24, relu|
+| Convolution | Depth 36, relu|
+| Convolution | Depth 48, relu|
+| Convolution | Depth 64, relu|
+| Convolution | Depth 64, relu|
+| Flatten| |
+| Fully Connected | 100 |
+| Dropout | 70% | 
+| Fully Connected | 50 |
+| Dropout | 85% |
+| Fully Connected | 10 |
+| output | 1 |
 
 
+Two dropout layers were introduced to the NVidia model to improve model robustness and reduce overfitting.
+
+The model used an adam optimized. The only tuned paramter was the number of ephocs which was set to 5. The training data was split into 80-20 ratio for training/validation.
+
+A generator was used to load images from the storage drive to avoid loading all images into memory directly.
+
+
+### Strategy
+The provided training data in conjunction with the above-described model was capable of smoothly driving the vehicle around the test track. The model could not drive in the second track. The model's response was very smooth which suggested that the model was overfitting the provided training data.
+
+To attempt a more robust approach, new training data was gathered using a controller input instead of mouse and keyboard. A total of 6 loops were gathered, 3 in the forward direction, and 3 in the backward direction. 1 loop in each direction was gathered in 'recovery' mode were the vehicle was driven with swaying pattern to simulate recovery towards the center. The new training data consisted of 7478 images from all 6 loops (in center channel).
+
+The model was trained on an NVidia GTX1070 video card with 5 epochs. The resulting model with the new data produced less smooth motion than the provided training data. Specifically, the new model gently swayed the vehicle from side to side. This suggests that the newly trained model attempted to position the vehicle such that the road edges became more clear in the view.
+
+The new model was capable of continuously remaining on the track indefinately. The model did not perform on the suplementally track.
